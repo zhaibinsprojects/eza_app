d56be9075c45f0f270a08f9dce1f6cbd821cc9ca
hlf <2096262844@qq.com>
2018-05-12 15:44:24 +0800

    修改接口（品类）

--- sanbang-front/src/main/java/com/sanbang/cata/controller/CataController.java
+++ sanbang-front/src/main/java/com/sanbang/cata/controller/CataController.java
@@ -20,12 +20,15 @@ public class CataController {
 	
 	@Autowired
 	private CataService cataService;
+	
+	
 
 	private static final String  view="/cata/";
 	
 	@RequestMapping("/init")
 	@ResponseBody
 	public Result getCataList(HttpServletRequest request){
+		System.out.println("我被访问了");
 		//查询一级分类列表
 		List list = cataService.getListForClass();
 		System.out.println(list);
@@ -35,5 +38,53 @@ public class CataController {
 		result.setObj(list);
 		return   result;
 	}
+	
+	//查询二级分类列表
+	public Result getCataList2(HttpServletRequest request,String parentsId){
+		Result result=Result.success();
+		
+		
+		
+		
+		return result;
+	}
+	
+	//查询三级分类列表
+	public Result getCataList3(HttpServletRequest request,String parentsId){
+		Result result=Result.success();
+		
+		
+		
+		
+		return result;
+	}
+	
+	
+	
+	
+	//自营、地区筛选、品类筛选
+	public Result listByAreaAndType(HttpServletRequest request,String parentsId){
+		Result result=Result.success();
+		
+		
+		
+		
+		return result;
+	}
+	
+	
+	
+	//其他筛选（颜色、形态、来源、用途、重要参数、燃烧等级、是否环保）
+	public Result listByOthers(HttpServletRequest request,String parentsId){
+		Result result=Result.success();
+		
+		
+		
+		
+		return result;
+	}
+	
+	
+	
 	
 }
