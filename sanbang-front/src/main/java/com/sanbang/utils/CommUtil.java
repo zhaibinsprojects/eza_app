b689cf2814b3f0f3e34e397804e8862014d3112d
zhaibinsprojects <1453672473@qq.com>
2018-06-08 17:11:07 +0800

    modify

--- sanbang-front/src/main/java/com/sanbang/utils/CommUtil.java
+++ sanbang-front/src/main/java/com/sanbang/utils/CommUtil.java
@@ -51,21 +51,19 @@ public class CommUtil {
 
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-	public static final String self_goods = "SELF_GOODS";// 自營商品
-	public static final String sample_goods = "SAMPLE_GOODS";
-	public static final String self_sample_goods = "SELF_SAMPLE_GOODS";// 自營樣品商品
+	public static final String sample_goods = "SAMPLE_GOODS";				//样品
+	
+	public static final String self_goods = "SELF_GOODS"; 					// 自营商品
+	public static final String self_sample_goods = "SELF_SAMPLE_GOODS";		// 自营样品
+	
+	public static final String match_goods = "MATCH_GOODS";					// 供应商商品
+	public static final String match_sample_goods = "MATCH_SAMPLE_GOODS";	// 供应商样品
 
-	public static final String match_sample_goods = "MATCH_SAMPLE_GOODS";// 供應商樣品商品
+	public static final String order_self_good = "ORDER_SELF_GOOD";			// 自营商品订单
+	public static final String order_match_good = "ORDER_MATCH_GOOD";		// 撮合商品订单
 
-	public static final String match_goods = "MATCH_GOODS";// 供應商商品
-
-	public static final String order_self_good = "ORDER_SELF_GOOD";// 自营商品订单
-
-	public static final String order_match_good = "ORDER_MATCH_GOOD";// 撮合商品订单
-
-	public static final String order_sample_good = "ORDER_SAMPLE_GOOD";// 样品商品订单
-
-	public static final String order_sample_match_good = "ORDER_SAMPLE_MATCH_GOOD";// 样品撮合商品订单
+	public static final String order_sample_good = "ORDER_SAMPLE_GOOD";		// 样品订单
+	public static final String order_sample_match_good = "ORDER_SAMPLE_MATCH_GOOD";// 样品撮合订单
 
 	/**
 	 * 数据字典中：性别>>男 的Code值
