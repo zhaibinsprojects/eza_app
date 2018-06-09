4453830c37deff1fcad25dfe75fc9c83265c9100
hlf <2096262844@qq.com>
2018-06-09 09:47:31 +0800

    修改接口

--- sanbang-front/src/main/java/com/sanbang/app/controller/AppGoodsController.java
+++ sanbang-front/src/main/java/com/sanbang/app/controller/AppGoodsController.java
@@ -20,7 +20,6 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.xhtmlrenderer.pdf.ITextFontResolver;
@@ -47,7 +46,6 @@ import com.sanbang.vo.DictionaryCode;
 
 import freemarker.template.Configuration;
 import freemarker.template.Template;
-import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 @Controller
