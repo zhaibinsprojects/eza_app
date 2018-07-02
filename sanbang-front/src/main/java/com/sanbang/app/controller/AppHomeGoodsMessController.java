11109570c88b1161100a28b06c3ce1b41a6c9352
zhaibinsprojects <1453672473@qq.com>
2018-07-02 17:37:02 +0800

    modify

--- sanbang-front/src/main/java/com/sanbang/app/controller/AppHomeGoodsMessController.java
+++ sanbang-front/src/main/java/com/sanbang/app/controller/AppHomeGoodsMessController.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.sanbang.bean.ezs_accessory;
 import com.sanbang.bean.ezs_column;
